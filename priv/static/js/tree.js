0609d722b477529f042fd2531a2b7ab44e23b2c6
Ilya Ashchepkov <koctep@gmail.com>
2014-02-28 14:59:38 +0700

    tree

--- priv/static/js/tree.js
+++ priv/static/js/tree.js
@@ -1,33 +1,85 @@
 angular.module('skuapso-tree', [])
-.directive('list', function($compile) {
+.directive('list', ['$compile', 'skuapso-data', function(compile, data) {
   var def = {};
 
-  def.$scope = {collapsed: '@collapsed'};
+  def.controller = ['$scope', function(scope) {
+    scope.collapsed = false;
 
-  def.controller = function($scope, $rootScope, $attrs) {
-    var $type = $attrs.list, $id = $attrs.listParentId || null;
-
-    Object.defineProperty($scope, '$childs', {
-      get: function() {
-        return $rootScope.$scopes[$type + '_' + $id];
-      }
-    });
-
-    $scope.collapsed = false;
-
-    $scope.$toggle = function() {
-      $scope.collapsed = !$scope.collapsed;
+    scope.toggle = function() {
+      console.debug('toggle');
+      this.collapsed = !this.collapsed;
     }
+  }];
+
+  def.link = function(scope, element) {
+    scope.$watch('node.childs.length==0', toggleImg);
+    scope.$watch('collapsed', toggleChilds, angular.equals);
+    scope.$watch('node.childs', rebuildChilds, angular.equals);
+
+    var toggleChilds = function(collapsed, wasCollapsed, scope) {
+      console.debug('toggle childs');
+      if (collapsed !== wasCollapsed)
+        element.find('>ul').toggle(100);
+      element.find('>img').attr('src',
+          collapsed ? '/static/img/button-closed.png' : '/static/img/button-open.png');
+    };
+
+    var toggleImg = function(childsCount, oldValue, scope) {
+      console.debug('toggle img');
+      var newElem, div;
+      if (childsCount) {
+        console.debug('removing img');
+        element.find('>img').remove();
+      } else {
+        console.debug('adding img');
+        div = element.find('>div');
+        newElem = (scope.collapsed) ? 'button-closed.png' : 'button-open.png';
+        newElem = '<img src="/static/img/' + newElem + '" ng-click="toggle()">';
+        console.debug('img is %o', newElem);
+        element.prepend(compile(newElem)(scope));
+      }
+    };
+
+    var rebuildChilds = function(childs, _childs, scope) {
+      if (!angular.isArray(childs)) return;
+      var ul, i, len = childs.length, newElem, newScope, item;
+      element.find('>ul').remove();
+      if (childs && childs.length > 0) {
+        ul = element.append('<ul></ul>').find('>ul');
+        for (i = 0; i < len; i++) {
+          item = scope.node.childs[i];
+          newElem = ul.append('<li ' + item.type + '="' + item.id + '"></li>').find('li:last-child');
+        }
+        compile(element.contents())(scope);
+        for (i = 0; i < len; i++) {
+          item = scope.node.childs[i];
+          if (data.childs(item).length) {
+            newElem = element.find('li[' + item.type + '=' + item.id +']');
+            newElem.attr('list', '');
+            newScope = scope.$new(true);
+            newScope.node = scope.node.childs[i];
+            newElem = compile(newElem)(newScope);
+            newElem = (scope.collapsed) ? 'button-closed.png' : 'button-open.png';
+            newElem = '<img src="/static/img/' + newElem + '" ng-click="toggle()">';
+            console.debug('img is %o', newElem);
+            element.prepend(compile(newElem)(scope));
+          }
+        }
+      }
+    };
+    rebuildChilds(scope.node.childs, [], scope);
   };
 
-  def.compile = function($element, $attrs) {
+  return def;
+
+  def.compile = function(element) {
     var c = {};
     var $type = $attrs.list , $id = $attrs.listParentId;
 		var $div = function($t, $i) {
 			return '<div data-type="' + $t + '" data-id' + '="' + $i + '" ' +$t + '="' + $i + '"></div>';
 		}
 
-    c['pre'] = function($scope) {
+    c['pre'] = function(scope) {
       $element.html('');
       if ($scope.$root.$item({type: $type, id: $id})) {
         $element.append($div($type, $id));
@@ -35,7 +87,7 @@ angular.module('skuapso-tree', [])
       $compile($element.addClass('list').contents())($scope);
     };
 
-    c['post'] = function($scope) {
+    c['post'] = function(scope) {
       var rebuildChilds = function(newValue, oldValue) {
         var i;
         var $newScope, $newElem;
@@ -87,8 +139,5 @@ angular.module('skuapso-tree', [])
     return c;
   };
 
-  def.link = function($scope, $element, $attrs) {
-  };
-
   return def;
-});
+}]);
