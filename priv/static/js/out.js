23d1d69d37373bdd2dbce790ca6c0775c5359c51
Ilya Ashchepkov <koctep@gmail.com>
2014-03-14 11:12:32 +0700

    update of Bert and String moved to out.js

--- priv/static/js/out.js
+++ priv/static/js/out.js
@@ -10,3 +10,35 @@ var contextMenuClick = function(ev) {
 };
 
 $('menu>command').on('click', contextMenuClick);
+
+$(function() {
+  BertClass.prototype.encode_string = function (Obj) {
+    var codes = [], i = 0, l = Obj.length;
+    for (i; i < l; i++) {
+      codes.push(Obj.charCodeAt(i));
+    }
+    return this.encode_array(codes);
+  };
+
+  BertClass.prototype.encode_char = function(Obj) {
+    var s = '', c = Obj.charCodeAt(0);
+    for(c; c; c = (c>>8)) {
+      s = String.fromCharCode(c % 256) + s;
+    }
+    return s;
+  };
+
+  Bert = new BertClass();
+
+  String.prototype.hex = function() {
+    function hex(c) {
+      return String.fromCharCode(c < 10 ? 48 + c : 87 + c);
+    }
+    var s = '', i = 0, l = this.length;
+    for (i; i < l; i++) {
+      s = s + hex(this.charCodeAt(i) >> 4);
+      s = s + hex(this.charCodeAt(i) & 0x0F);
+    }
+    return s;
+  }
+});
