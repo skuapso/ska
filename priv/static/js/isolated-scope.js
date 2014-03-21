2acc8ce8a7b94b229c1e40bbdd1010b069b48106
Ilya Ashchepkov <koctep@gmail.com>
2014-03-21 11:02:51 +0700

    isolated scope added digest service (rename $digest, pending digest scopes)

--- priv/static/js/isolated-scope.js
+++ priv/static/js/isolated-scope.js
@@ -1,9 +1,60 @@
 'use strict';
 
 angular.module('isolated-scope', [])
+.service('digest', [
+    '$rootScope',
+    '$exceptionHandler',
+    function(root, exception) {
+      var scopes = [];
+      var ttl = 10;
+
+      this.add = function(scope) {
+        if (scopes.length > ttl) {
+          exception('isolated-scope digest ttl excided ', ttl);
+          return this;
+        }
+        scopes.push(scope);
+        setTimeout(run, 0);
+        return this;
+      };
+
+      function run() {
+        if (scopes.length == 0) return;
+        if (root.$$phase || scopes[0].$$phase) {return;}
+        var scope = scopes.shift();
+        scope.$digest();
+        setTimeout(run, 0);
+      };
+
+      this.install = function(scope) {
+        function newDigest() {
+          var i = 0, l = scopes.length;
+          if (this.$$phase) {
+            for (i; i < l; i++)
+              if (scopes[i] === this)
+                return;
+            scopes.push(this);
+            return;
+          }
+          this.$$oldDigest();
+        };
+
+        scope.$$oldDigest = scope.$digest;
+        scope.$digest = newDigest;
+      }
+
+      tmp = scopes;
+      this.install(root);
+//      setTimeout(run, 100);
+    }
+])
 .provider('isolatedScope', ['$rootScopeProvider', function(rootProvider) {
-  this.$get = ['$injector', '$rootScope', function(injector) {
-    return function() {return injector.invoke(rootProvider.$get)};
+  this.$get = ['$injector', 'digest', function(injector, digest) {
+    return function() {
+      var scope = injector.invoke(rootProvider.$get);
+      digest.install(scope);
+      return scope;
+    };
   }];
 }])
 /*.run(['$rootScope', function(root, skRoot) {
