e9a6d900e953f0248d9198e82a7b5bfb417d3cf6
Ilya Ashchepkov <koctep@gmail.com>
2014-04-22 16:11:22 +0700

    js: map from cloudmade to osm

--- priv/static/js/map.js
+++ priv/static/js/map.js
@@ -5,17 +5,12 @@ angular.module('skuapso-map', [])
     function() {
       this.create = function(element) {
         this.map = L.map(element).setView([55.75, 80.17], 7);
-        var cloudMadeApiKey = '548f0a06c2ed4b34aefe2a2a5bca5c08',
-          cloudMadeUrl = 'http://{s}.tile.cloudmade.com/{apiKey}/{styleId}/256/{z}/{x}/{y}.png',
-          cloudMadeAttr = 'Map data &copy; '
-          + '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
-          + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '
-          + 'Imagery © <a href="http://cloudmade.com">CloudMade</a>';
-        L.tileLayer(cloudMadeUrl, {
-          apiKey: cloudMadeApiKey,
-          styleId: 22677,
-          attribution: cloudMadeAttr,
-          maxZoom: 18
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          name: 'OpenStreetMap',
+          layerOptions: {
+            attribution: '© OpenStreetMap contributors',
+            continuousWorld: true
+          }
         })
         .addTo(this.map);
       }
