295866bd5dde8e3f9b521cc68246af39c2c18728
Ilya Ashchepkov <koctep@gmail.com>
2014-04-24 09:02:30 +0700

    js:test added probj to print object

--- priv/static/js/test.js
+++ priv/static/js/test.js
@@ -1,4 +1,20 @@
 'use strict';
+
 var tmp;
 angular.module('skuapso.test', ['skuapso', 'skuapso-init'])
 ;
+
+var objs = [];
+function probj(obj, nesting) {
+  var id, n = typeof nesting === 'undefined' ? 0 : nesting;
+  objs.push(obj);
+  for (id in obj) {
+    console.debug('%o, %o: %o', n, id, obj[id]);
+    if (angular.isArray(obj[id])) continue;
+    if (angular.isString(obj[id])) continue;
+    if (objs.indexOf(obj[id]) == -1) {
+      probj(obj[id], n + 1);
+    }
+  }
+  objs.pop();
+};
