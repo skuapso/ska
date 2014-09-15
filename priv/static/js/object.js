b135ff3cac5f5a83d5f0be8c0304918607a18372
Ilya Ashchepkov <koctep@gmail.com>
2014-09-15 12:28:02 +0700

    added L.Track, overwritten L.LatLng, added object.track point img for track

--- priv/static/js/object.js
+++ priv/static/js/object.js
@@ -18,15 +18,17 @@ angular.module('skuapso-init')
           var $to   = filter('date')(root.controls.toDateTime, 'psql');
           var $url = '/object/' + this.id + '/track/'
                     + $from + '/' + $to;
-          if (root.controls.sensor) $url += '/sensor/1/>/70';
           http.get($url).success(function(data) {
-            var i, line, lines;
+            var i, line, lls = [];
             for (i = 0; i < data.length; i++) {
-              line = L.polyline(data[i].track);
+              line = L.track(data[i].track);
               line.bindPopup(data[i].object_id + '<br>' + data[i].min + '>>' + data[i].max);
               line.addTo(map.map);
+              lls = lls.concat(line.getLatLngs());
             }
-            //          map.fitBounds(lines.getBounds());
+            if (lls.length > 0) {
+              map.map.fitBounds(new L.LatLngBounds(lls));
+            }
           });
         };
         o.mileage = function() {
